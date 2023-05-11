73cb59e730d0e6303c94ef5bdf3315ce714a9573
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-05-10 17:14:46 -0700

    feat: Show the movies displayed on Dom as well as abilitiy to view a single movies details

--- src/App/App.js
+++ src/App/App.js
@@ -2,21 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import movieData from '../movie-data';
 import Movies from '../Movies/Movies';
+import Movie from '../Movie/Movie';
 import Header from '../Header/Header';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = movieData;
+    this.state = {
+      allMovies: movieData,
+      specificMovie: null
+    };
   }
-
+  displaySingleMovie = (id) => {
+    const filteredMovie = this.state.allMovies.movies.filter(movie => movie.id === id)
+    this.setState({
+      allMovies: null,
+      specificMovie: filteredMovie
+    })
+  }
+  goBackToHome = () => {
+    this.setState({
+      allMovies: movieData,
+      specificMovie: null
+    })
+  }
   render() {
-    return(
-      <div>
-        <Header />
-        <Movies movies={this.state.movies}/>
-      </div>
-    )
+    if (!this.state.specificMovie) {
+      return(
+        <div>
+          {/* <Header /> */}
+          <Movies movies={this.state.allMovies.movies} displaySingleMovie={this.displaySingleMovie}/>
+        </div>
+      )
+    } else {
+      return (
+        <div> 
+          <button className='leave-button' onClick={this.goBackToHome}></button>
+          <Movie movie={this.state.specificMovie} />
+        </div>
+      )
+    }
   }
 }
 
---
+++ src/Movie/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+import "./Movie.css"
+
+
+const Movie = (props) => {
+    console.log(props.movie)
+    return (
+        <div className="single-movie">
+            <img classname="single-movie-img"src={props.movie[0].backdrop_path} height="100px" />
+        </div>
+    )
+}
+export default Movie
