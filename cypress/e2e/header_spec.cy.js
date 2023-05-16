c53ac99bd90ea2f57e66338d66aae7d6728a5044
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-05-16 16:41:26 -0700

    shows the propa cypress testing added for sake of user stories

--- cypress/e2e/header_spec.cy.js
+++ cypress/e2e/header_spec.cy.js
@@ -1,5 +1,30 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Header', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('Loading.....').should('not.exist')
+  })
+
+  it('Should have a logo', () => {
+    cy.get('.header').get(".logo")
+  })
+
+  it('Should be able to search for a movie by its title', () => {
+    cy.get(".search-bar").type('woman')
+      .get('.movie-posters').should('have.length', 1)
+      .get('.movie-posters').first().click()
+      .url().should('include', '/724495')
+  })
+
+  it("Should be able to sort from lowest to highest", () => {
+    cy.get('#ratings').select('lowest')
+    .get('.movie-posters').first().click()
+    .url().should('include', '/829799')
+  })
+
+  it("Should be able to sort from highest to lowest", () => {
+    cy.get('#ratings').select('lowest')
+    .get('#ratings').select('highest')
+    .get('.movie-posters').first().click()
+    .url().should('include', '/979924')
   })
 })
--- cypress/e2e/movies_spec.cy.js
+++ cypress/e2e/movies_spec.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Movies', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('Loading.....').should('not.exist')
+  })
+
+  it('should display movie posters', () => {
+    cy.get('.movie-posters-section').should('exist')
+  })
+  it('Should display a list of movies', () => {
+    cy.get('.movie-posters').should('have.length', 40)
+  })
+  it("SHould be able to click a movie poster", () => {
+    cy.get('.movie-posters').first().click();
+    cy.url().should('include', '/436270')
   })
 })
