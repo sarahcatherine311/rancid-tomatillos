edde74aef1818318e7f02126cb9f13a2af005dec
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-05-16 15:04:11 -0700

    shows the ability to go back to home page from single movie view

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import "./Footer.css";
+import { Link } from 'react-router-dom'
 
 const Footer = (props) => {
   return (
     <footer >
-      <img onClick={props.goBackToHome} className="footer-logo" src="https://i.ibb.co/8bGjmZw/Untitled-Artwork.png" alt="Rancid Tomatillo Logo"/>
+      <Link to={'/'}>
+         <img onClick={props.goBackToHome} className="footer-logo" src="https://i.ibb.co/8bGjmZw/Untitled-Artwork.png" alt="Rancid Tomatillo Logo"/>
+      </Link>
       {/* <button onClick={props.goBackToHome}>Return to Main Page</button> */}
     </footer>
   );
